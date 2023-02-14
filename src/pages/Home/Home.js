978424e7dfc9b5d7083bde784b55f87d683ffffd
seyton35 <prosto99-00@mail.ru>
2023-02-15 00:06:45 +0300

    init

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import {Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default function Home() {
+
+    return(
+        <div className="container">
+            <Button variant='primary'>brn</Button>
+        </div>
+    )
+}
+
