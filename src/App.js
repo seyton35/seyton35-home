fe036a9e54fe4f902f79654e06dc0ed2f1d3bc0c
seyton35 <prosto99-00@mail.ru>
2023-02-15 00:05:54 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './Components/Header';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
